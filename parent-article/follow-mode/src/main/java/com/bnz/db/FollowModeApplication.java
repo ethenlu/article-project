4e9122345923af9e8f76fb828aa98f3009d1da6c
DB <2502523450@qq.com>
2020-09-25 17:12:48 +0800

    创建关注模块，放入父工程下

---
+++ parent-article/follow-mode/src/main/java/com/bnz/db/FollowModeApplication.java
@@ -0,0 +1,13 @@
+package com.bnz.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FollowModeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FollowModeApplication.class, args);
+    }
+
+}
---
+++ parent-article/follow-mode/src/test/java/com/bnz/db/FollowModeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnz.db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FollowModeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
